095b01c675dedd198209a017be9cf60cb5d0f471
xjncx <joncy4mm@gmail.com>
2022-08-25 17:48:49 +0300

    123

--- CodeRecomendation/CodeRecomendation/Assets/Scripts/HouseAlarm.cs
+++ CodeRecomendation/CodeRecomendation/Assets/Scripts/HouseAlarm.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class HouseAlarm : MonoBehaviour
 {
     [SerializeField] private AudioSource _alarmSound;
-
+    private WaitForSeconds _waitForOneSecond = new WaitForSeconds(1);
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent<Player>(out Player player))
         {
-            Debug.Log("Вошел");
-            StartCoroutine(ChangeVolume());
+            _alarmSound.Play();
+            StartCoroutine(ChangeVolume(1));
         }
     }
 
@@ -19,19 +19,22 @@ public class HouseAlarm : MonoBehaviour
     {
         if (collision.TryGetComponent<Player>(out Player player))
         {
-            _alarmSound.Stop();
+            StopCoroutine(ChangeVolume(1));
+            StartCoroutine(ChangeVolume(0));
         }
     }
 
-    private IEnumerator ChangeVolume()
+    private IEnumerator ChangeVolume(float direction)
     {
-        _alarmSound.Play();
-
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 5; i++)
         {
-            _alarmSound.volume = Mathf.MoveTowards(0, 1, 0.1f * Time.deltaTime);
-            Debug.Log("Подсчет цикла");
-            yield return null;
+            _alarmSound.volume = Mathf.MoveTowards(_alarmSound.volume, direction, 0.2f);
+            yield return _waitForOneSecond;
+        }
+
+        if (direction == 0)
+        {
+            _alarmSound.Stop();
         }
     }
 }
