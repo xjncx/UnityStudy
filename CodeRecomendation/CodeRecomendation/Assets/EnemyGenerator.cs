e8335a5a1fa6cbcb1d24759c35f2ffaa1dcf708c
xjncx <joncy4mm@gmail.com>
2022-08-26 04:12:53 +0300

    updated

---
+++ CodeRecomendation/CodeRecomendation/Assets/EnemyGenerator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    [SerializeField] private Enemy _prefab;
+    [SerializeField] private List<Transform> _pointsToSpawn;
+
+    private bool isSpawning = true;
+    private WaitForSeconds _waitForTwoSeconds = new WaitForSeconds(2);
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+    private Enemy GetNewInstance()
+    {
+            Vector3 prefabPosition = _pointsToSpawn[Random.Range(0, _pointsToSpawn.Count)].position;
+            return Instantiate(_prefab, prefabPosition, Quaternion.identity);
+    }
+
+    private IEnumerator Spawn()
+    {
+        while (isSpawning)
+        {
+            GetNewInstance();
+            yield return _waitForTwoSeconds;
+        }
+    }
+}
